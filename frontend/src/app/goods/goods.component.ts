1413c7a2925f44c36c6fedcf110d33edfd6205de
AudonZhang <audonzhang@gmail.com>
2024-03-31 21:18:31 +0800

    finish goods component

--- frontend/src/app/goods/goods.component.ts
+++ frontend/src/app/goods/goods.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { Goods } from '../interfaces/goods';
+import { GoodsService } from '../services/goods.service';
 
 @Component({
   selector: 'app-goods',
   templateUrl: './goods.component.html',
   styleUrls: ['./goods.component.css'],
 })
-export class GoodsComponent {
-  constructor(private router: Router, private route: ActivatedRoute) {
+export class GoodsComponent implements OnInit {
+  goodss: Goods[] = [];
+  options1: any;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private goodService: GoodsService
+  ) {
     // 进入子页面修改用户信息时，不显示该页面内容
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
@@ -18,5 +26,50 @@ export class GoodsComponent {
   }
   isChildRouteActive(): boolean {
     return this.route.children.length > 0;
+  }
+
+  setupChart(): void {
+    this.options1 = {
+      legend: {
+        text: '货物数量统计',
+        top: 'bottom',
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)',
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          mark: { show: true },
+          dataView: { show: true, readOnly: false },
+          restore: { show: true },
+          saveAsImage: { show: true },
+        },
+      },
+      series: [
+        {
+          name: '货物信息',
+          type: 'pie',
+          radius: [50, 250],
+          center: ['50%', '50%'],
+          roseType: 'area',
+          itemStyle: {
+            borderRadius: 8,
+          },
+          data: this.goodss.map((goods) => ({
+            value: goods.goodsAmount,
+            name: goods.goodsName,
+          })),
+        },
+      ],
+    };
+  }
+
+  ngOnInit(): void {
+    this.goodService.getAllGoods().subscribe((res) => {
+      this.goodss = res;
+      this.setupChart();
+    });
   }
 }
